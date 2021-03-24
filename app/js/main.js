550773df5ab9ca6defebf53b0c8ce815e59000f3
makeshka03 <terror.q2003@gmail.com>
2021-03-24 22:21:52 +0500

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.wrapper__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        speed: 500
+    });
+});
